82a98b1eac0f18f139d91351b81f86635708bb9a
Jesús Neri <jesusneri1024@outlook.com>
2025-05-12 14:50:08 -0600

    feat: triangle on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,65 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+std::string loadFile(const char *path)
+{
+    std::ifstream file(path);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+GLuint compileShader(GLenum type, const char *source)
+{
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+    int success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        char info[512];
+        glGetShaderInfoLog(shader, 512, nullptr, info);
+        std::cerr << "Shader compile error:\n"
+                  << info << std::endl;
+    }
+    return shader;
+}
+
+GLuint createShaderProgram(const char *vertexPath, const char *fragmentPath)
+{
+    std::string vertCode = loadFile(vertexPath);
+    std::string fragCode = loadFile(fragmentPath);
+    GLuint vert = compileShader(GL_VERTEX_SHADER, vertCode.c_str());
+    GLuint frag = compileShader(GL_FRAGMENT_SHADER, fragCode.c_str());
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vert);
+    glAttachShader(program, frag);
+    glLinkProgram(program);
+
+    int success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        char info[512];
+        glGetProgramInfoLog(program, 512, nullptr, info);
+        std::cerr << "Shader link error:\n"
+                  << info << std::endl;
+    }
+
+    glDeleteShader(vert);
+    glDeleteShader(frag);
+    return program;
+}
 
 int main()
 {
     if (!glfwInit())
-    {
-        std::cerr << "Failed to init GLFW\n";
         return -1;
-    }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
@@ -17,31 +68,53 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "My Engine (Neri)", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "My Engine", nullptr, nullptr);
     if (!window)
-    {
-        std::cerr << "Failed to create GLFW window\n";
-        glfwTerminate();
         return -1;
-    }
-
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cerr << "Failed to initialize GLAD\n";
         return -1;
-    }
 
-    glClearColor(0.1f, 0.2f, 0.3f, 1.0f); // Azul oscuro
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // izquierda
+        0.5f, -0.5f, 0.0f,  // derecha
+        0.0f, 0.5f, 0.0f    // arriba
+    };
+
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    GLuint shader = createShaderProgram("src/shader.vert", "src/shader.frag");
 
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shader);
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
